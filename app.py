be2da6b7f1afe2df78c3bf8569a510ce2dd31290
rororyo <ryonathaniel05@gmail.com>
2026-05-10 18:51:25 +0700

    feat: added cors for prod

--- app.py
+++ app.py
@@ -6,10 +6,21 @@ from datetime import datetime
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}},
-         methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-         allow_headers=["Content-Type", "Authorization"],
-         supports_credentials=True)
+
+    CORS(
+        app,
+        resources={
+            r"/*": {
+                "origins": [
+                    "http://localhost:3000",
+                    "https://sigap-two.vercel.app"
+                ]
+            }
+        },
+        methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+        allow_headers=["Content-Type", "Authorization"],
+        supports_credentials=True
+    )
 
     app.register_blueprint(fraud_bp)
 
